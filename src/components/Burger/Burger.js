ecc3b4f0e5b2fdcc775216ead8271b819ff19843
Amanda Irazabal <amanda.irazabal@gmail.com>
2021-01-14 18:30:11 -0500

    Transformed obj of ingredients to array and added ing state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,17 @@ import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
+  // Turn object of key value pairs into an array
+  const transformedIng = Object.keys(props.ingredients).map((igKey) => {
+    return [...Array(props.ingredients[igKey])].map((_, i) => {
+      return <BurgerIngredient key={igKey + i} type={igKey}></BurgerIngredient>;
+    });
+  });
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top"></BurgerIngredient>
-      <BurgerIngredient type="cheese"></BurgerIngredient>
-      <BurgerIngredient type="meat"></BurgerIngredient>
+      {transformedIng}
       <BurgerIngredient type="bread-bottom"></BurgerIngredient>
     </div>
   );
