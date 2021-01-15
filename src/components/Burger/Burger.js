70632dcc78d697fa5a57c59603deb7c069aeba02
Amanda Irazabal <amanda.irazabal@gmail.com>
2021-01-14 19:01:46 -0500

    Added build controls and styling

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,23 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
   // Turn object of key value pairs into an array
-  const transformedIng = Object.keys(props.ingredients).map((igKey) => {
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return <BurgerIngredient key={igKey + i} type={igKey}></BurgerIngredient>;
-    });
-  });
+  let transformedIng = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return (
+          <BurgerIngredient key={igKey + i} type={igKey}></BurgerIngredient>
+        );
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (transformedIng.length === 0) {
+    transformedIng = <p>Please start adding ingredients!</p>;
+  }
+
+  console.log(transformedIng);
 
   return (
     <div className={classes.Burger}>
